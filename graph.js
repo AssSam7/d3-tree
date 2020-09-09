905e609b468f88298f721037f2aadbe1a78bd8fe
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-10 02:31:49 +0530

    Tree generator

--- graph.js
+++ graph.js
@@ -6,10 +6,31 @@ const svg = d3
   .select(".canvas")
   .append("svg")
   .attr("width", dims.width + 100)
-  .attr("height", dim.height + 100);
+  .attr("height", dims.height + 100);
 
 // Graph group
 const graph = svg.append("g").attr("transform", "translate(50, 50)");
+
+// Data stratify
+const stratify = d3
+  .stratify()
+  .id((d) => d.name)
+  .parentId((d) => d.parent);
+
+// Tree Generator
+const tree = d3.tree().size([dims.width, dims.height]);
+
+// Update function to re-render the visualizations
+const update = (data) => {
+  // Get updated root node data
+  const rootNode = stratify(data);
+
+  // Tree data from the tree generator
+  const treeData = tree(rootNode);
+
+  // Joining the data (Enter selection)
+  const nodes = graph.selectAll(".node").data(tree.descendants());
+};
 
 // data and firestore hook-up
 let data = [];
@@ -34,5 +55,5 @@ db.collection("employees").onSnapshot((res) => {
     }
   });
 
-  console.log(data);
+  update(data);
 });
